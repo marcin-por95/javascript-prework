e6a0c1ed380112b085437d6d696aaa053779c90b
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-03-04 12:38:37 +0100

    Add game score script

--- js/script.js
+++ js/script.js
@@ -1,44 +1,68 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
- console.log('Wylosowana liczba to: ' + randomNumber);
+console.log('Wylosowana liczba to: ' + randomNumber);
 
 
- let computerMove = 'nieznany ruch';
- 
- // COMPUTER MOVE SCRIPT ---------------------------------------------------------
- if(randomNumber == 1){
-   
-   computerMove = 'rock';
- }
-   else if (randomNumber == 2){
-     computerMove = 'paper';
-     
-   }
-   else if (randomNumber == 3){
-     computerMove = 'scizzors';
-    
-   }
+let computerMove = 'unknown move';
 
- console.log('move computer is: ' + computerMove);
- printMessage('move computer is: ' + computerMove);
- // PLAYER MOVE SCRIPT -----------------------------------------------------------
- let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+// COMPUTER MOVE SCRIPT ---------------------------------------------------------
+if (randomNumber == 1) {
 
- console.log('Gracz wpisał: ' + playerInput);
- 
- let playerMove = 'nieznany ruch';
- 
- if(playerInput == '1'){
-   playerMove = 'rock';
- }
- if(playerInput == '2'){
-   playerMove = 'paper';
- }
- if(playerInput == '3'){
-   playerMove = 'scizzors';
- }
- 
- printMessage('Your move is: ' + playerMove);
- 
- if(randomNumber == playerInput);
- printMessage('Game score is: Remis');
+    computerMove = 'rock';
+}
+else if (randomNumber == 2) {
+    computerMove = 'paper';
+
+}
+else if (randomNumber == 3) {
+    computerMove = 'scizzors';
+
+}
+
+console.log('Move computer is: ' + computerMove);
+printMessage('Move computer is: ' + computerMove);
+// PLAYER MOVE SCRIPT -----------------------------------------------------------
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'Unknown move';
+
+if (playerInput == '1') {
+    playerMove = 'rock';
+}
+if (playerInput == '2') {
+    playerMove = 'paper';
+}
+if (playerInput == '3') {
+    playerMove = 'scizzors';
+}
+printMessage('Your move is: ' + playerMove);
+
+//Game score -------------------------------------------------------------------------
+if (computerMove == 'paper' && playerMove == 'rock') {
+    printMessage('Computer WIN!');
+}
+if (computerMove == 'scizzors' && playerMove == 'paper') {
+    printMessage('Computer WIN!');
+}
+if (computerMove == 'rock' && playerMove == 'scizzors') {
+    printMessage('Computer WIN!');
+}
+//player score ------------------------------------------------------------------------
+if (computerMove == 'paper' && playerMove == 'scizzors') {
+    printMessage('You WIN!');
+}
+if (computerMove == 'scizzors' && playerMove == 'rock') {
+    printMessage('You WIN!');
+}
+if (computerMove == 'rock' && playerMove == 'paper') {
+    printMessage('You WIN!');
+}
+
+if (computerMove == playerMove) {
+    printMessage('Remis!');
+}
+if (playerInput == playerMove) {
+    printMessage('Unknown move!');
+}
