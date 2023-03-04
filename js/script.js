47c0a13a577fe8d25d302f5376fbae4bd9903f26
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-03-04 12:21:32 +0100

    Add player move

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
-let computerMove = `kamień`;
-let playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+ console.log('Wylosowana liczba to: ' + randomNumber);
+
+
+ let computerMove = 'nieznany ruch';
+ 
+ // COMPUTER MOVE SCRIPT ---------------------------------------------------------
+ if(randomNumber == 1){
+   
+   computerMove = 'rock';
+ }
+   else if (randomNumber == 2){
+     computerMove = 'paper';
+     
+   }
+   else if (randomNumber == 3){
+     computerMove = 'scizzors';
+    
+   }
+
+ console.log('move computer is: ' + computerMove);
+ printMessage('move computer is: ' + computerMove);
+ // PLAYER MOVE SCRIPT -----------------------------------------------------------
+ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+ console.log('Gracz wpisał: ' + playerInput);
+ 
+ let playerMove = 'nieznany ruch';
+ 
+ if(playerInput == '1'){
+   playerMove = 'rock';
+ }
+ if(playerInput == '2'){
+   playerMove = 'paper';
+ }
+ if(playerInput == '3'){
+   playerMove = 'scizzors';
+ }
+ 
+ printMessage('Your move is: ' + playerMove);
+ 
+ if(randomNumber == playerInput);
+ printMessage('Game score is: Remis');
