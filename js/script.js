22a97ac2235dc21b967f2006b869c5bb6bc74f20
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-03-06 09:49:32 +0100

    Add counting for players & fix problems with buttons

--- js/script.js
+++ js/script.js
@@ -1,64 +1,56 @@
-function playGame(){
+let playerScore = 0;
+let computerScore = 0;
+
+function playGame(playerInput){ // playerInput = 1
+
+    function getMoveName(argMoveId){ // argMoveId = 2
+        if(argMoveId == 1){
+            return 'rock';
+        }
+        else if (argMoveId == 2){
+            return 'paper';
+        }
+        else if (argMoveId == 3){
+            return 'scissors';
+        }
+    }
+
+    function displayResult(argComputerMove, argPlayerMove){
+        console.log('wywołano displayResult');
+        printMessage('Computer choose ' + argComputerMove + ', and player choose ' + argPlayerMove);
+        if((argComputerMove == 'rock' && argPlayerMove == 'paper') || (argPlayerMove == 'scissors' && argComputerMove == 'paper') || (argPlayerMove == 'rock' && argComputerMove == 'scissors')){
+            printMessage('Player win this round.');
+            playerScore = playerScore + 1;
+        }
+        else if( argComputerMove == argPlayerMove) {
+            printMessage("'It's a draw !");
+        }
+        else {
+            printMessage('Computer win this round.');
+            computerScore = computerScore + 1;
+        }
+        console.log(playerScore, computerScore);
+        printMessage(playerScore + " : " + computerScore);
+    }
+    
     clearMessages();
 
-    let argPlayerMove = (getMoveName);
-    /*
-    playerInput
-    */
-//randomize 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('The drawn number is : ' + randomNumber);
-//Computer move
-let argComputerMove = getMoveName(randomNumber);
-//Player move
-/*
-let playerInput = prompt('Make your move !  1 = Rock  2 = Paper  3 = Scissors.');
+    let argPlayerMove = getMoveName(playerInput); // 1 -> 'rock'
 
-console.log('Player number is: ' + playerInput);
+    //randomize 
+    let randomNumber = Math.floor(Math.random() * 3 + 1); // 2
+    let argComputerMove = getMoveName(randomNumber); // 2 -> 'paper'
 
-let argPlayerMove = getMoveName(playerInput);
-*/
-function getMoveName(argMoveId){
-    if(argMoveId == 1){
-        return 'rock';
-    }
-    else if (argMoveId == 2){
-        return 'paper';
-    }
-    else if (argMoveId == 3){
-        return 'scissors';
-    }
-    else {
-        return 'unknown move';
-    }
+    displayResult(argComputerMove, argPlayerMove);
 }
-console.log(argComputerMove);
-console.log(argPlayerMove);
 
- function displayResult(argComputerMove, argPlayerMove){
-     console.log('wywołano displayResult');
-     printMessage('Computer choose ' + argComputerMove + ', and player choose ' + argPlayerMove);
-     if( argComputerMove == 'rock' && argPlayerMove == 'paper' + argComputerMove == 'paper' && argPlayerMove == 'scissors' + argComputerMove == 'scissors' && argPlayerMove == 'rock'){
-         printMessage('Player win this round.');
-     }
-     else if( argComputerMove == 'rock' && argPlayerMove =='rock'){
-         printMessage("'It's a draw !");
-     }
-     else if(argPlayerMove == 'unknown move'){
-         printMessage('Unknown player move');
-     }
-     else {
-         printMessage('Computer win this round.');
-     }
- }
- displayResult(argComputerMove, argPlayerMove);
-}
 document.getElementById('play-rock').addEventListener('click', function(){
     playGame(1);
-  });
-  document.getElementById('play-paper').addEventListener('click', function(){
+});
+document.getElementById('play-paper').addEventListener('click', function(){
     playGame(2);;
-  });
-  document.getElementById('play-scissors').addEventListener('click', function(){
+});
+document.getElementById('play-scissors').addEventListener('click', function(){
     playGame(3);;
-  });
+});
+
